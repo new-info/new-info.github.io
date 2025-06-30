808c78bebf01625754069612c08873f3390da2f9
new-info1 <to1249485588@outlook.com>
2025-07-01 02:02:25 +0800

    feat: 优化文件查看权限

--- assets/js/auth-helper.js
+++ assets/js/auth-helper.js
@@ -1,12 +1,108 @@
 // 认证帮助函数
-(function() {
+(function () {
     // MD5哈希函数实现
     function md5(input) {
         // 创建一个变量用于存储MD5函数的引用
         let md5Function;
 
         // 引用Blueimp MD5实现
-        !function(n){"use strict";function t(n,t){var r=(65535&n)+(65535&t);return(n>>16)+(t>>16)+(r>>16)<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function e(n,e,o,u,c,f){return t(r(t(t(e,n),t(u,f)),c),o)}function o(n,t,r,o,u,c,f){return e(t&r|~t&o,n,t,u,c,f)}function u(n,t,r,o,u,c,f){return e(t&o|r&~o,n,t,u,c,f)}function c(n,t,r,o,u,c,f){return e(t^r^o,n,t,u,c,f)}function f(n,t,r,o,u,c,f){return e(r^(t|~o),n,t,u,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;var e,i,a,d,h,l=1732584193,g=-271733879,v=-1732584194,m=271733878;for(e=0;e<n.length;e+=16)i=l,a=g,d=v,h=m,g=f(g=f(g=f(g=f(g=c(g=c(g=c(g=c(g=u(g=u(g=u(g=u(g=o(g=o(g=o(g=o(g,v=o(v,m=o(m,l=o(l,g,v,m,n[e],7,-680876936),g,v,n[e+1],12,-389564586),l,g,n[e+2],17,606105819),m,l,n[e+3],22,-1044525330),v=o(v,m=o(m,l=o(l,g,v,m,n[e+4],7,-176418897),g,v,n[e+5],12,1200080426),l,g,n[e+6],17,-1473231341),m,l,n[e+7],22,-45705983),v=o(v,m=o(m,l=o(l,g,v,m,n[e+8],7,1770035416),g,v,n[e+9],12,-1958414417),l,g,n[e+10],17,-42063),m,l,n[e+11],22,-1990404162),v=o(v,m=o(m,l=o(l,g,v,m,n[e+12],7,1804603682),g,v,n[e+13],12,-40341101),l,g,n[e+14],17,-1502002290),m,l,n[e+15],22,1236535329),v=u(v,m=u(m,l=u(l,g,v,m,n[e+1],5,-165796510),g,v,n[e+6],9,-1069501632),l,g,n[e+11],14,643717713),m,l,n[e],20,-373897302),v=u(v,m=u(m,l=u(l,g,v,m,n[e+5],5,-701558691),g,v,n[e+10],9,38016083),l,g,n[e+15],14,-660478335),m,l,n[e+4],20,-405537848),v=u(v,m=u(m,l=u(l,g,v,m,n[e+9],5,568446438),g,v,n[e+14],9,-1019803690),l,g,n[e+3],14,-187363961),m,l,n[e+8],20,1163531501),v=u(v,m=u(m,l=u(l,g,v,m,n[e+13],5,-1444681467),g,v,n[e+2],9,-51403784),l,g,n[e+7],14,1735328473),m,l,n[e+12],20,-1926607734),v=c(v,m=c(m,l=c(l,g,v,m,n[e+5],4,-378558),g,v,n[e+8],11,-2022574463),l,g,n[e+11],16,1839030562),m,l,n[e+14],23,-35309556),v=c(v,m=c(m,l=c(l,g,v,m,n[e+1],4,-1530992060),g,v,n[e+4],11,1272893353),l,g,n[e+7],16,-155497632),m,l,n[e+10],23,-1094730640),v=c(v,m=c(m,l=c(l,g,v,m,n[e+13],4,681279174),g,v,n[e],11,-358537222),l,g,n[e+3],16,-722521979),m,l,n[e+6],23,76029189),v=c(v,m=c(m,l=c(l,g,v,m,n[e+9],4,-640364487),g,v,n[e+12],11,-421815835),l,g,n[e+15],16,530742520),m,l,n[e+2],23,-995338651),v=f(v,m=f(m,l=f(l,g,v,m,n[e],6,-198630844),g,v,n[e+7],10,1126891415),l,g,n[e+14],15,-1416354905),m,l,n[e+5],21,-57434055),v=f(v,m=f(m,l=f(l,g,v,m,n[e+12],6,1700485571),g,v,n[e+3],10,-1894986606),l,g,n[e+10],15,-1051523),m,l,n[e+1],21,-2054922799),v=f(v,m=f(m,l=f(l,g,v,m,n[e+8],6,1873313359),g,v,n[e+15],10,-30611744),l,g,n[e+6],15,-1560198380),m,l,n[e+13],21,1309151649),v=f(v,m=f(m,l=f(l,g,v,m,n[e+4],6,-145523070),g,v,n[e+11],10,-1120210379),l,g,n[e+2],15,718787259),m,l,n[e+9],21,-343485551),l=t(l,i),g=t(g,a),v=t(v,d),m=t(m,h);return[l,g,v,m]}function a(n){var t,r="",e=32*n.length;for(t=0;t<e;t+=8)r+=String.fromCharCode(n[t>>5]>>>t%32&255);return r}function d(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1)r[t]=0;var e=8*n.length;for(t=0;t<e;t+=8)r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32;return r}function h(n){return a(i(d(n),8*n.length))}function l(n,t){var r,e,o=d(n),u=[],c=[];for(u[15]=c[15]=void 0,o.length>16&&(o=i(o,8*n.length)),r=0;r<16;r+=1)u[r]=909522486^o[r],c[r]=1549556828^o[r];return e=i(u.concat(d(t)),512+8*t.length),a(i(c.concat(e),640))}function g(n){var t,r,e="";for(r=0;r<n.length;r+=1)t=n.charCodeAt(r),e+="0123456789abcdef".charAt(t>>>4&15)+"0123456789abcdef".charAt(15&t);return e}function v(n){return unescape(encodeURIComponent(n))}function m(n){return h(v(n))}function p(n){return g(m(n))}function s(n,t){return l(v(n),v(t))}function C(n,t){return g(s(n,t))}function A(n,t,r){return t?r?s(t,n):C(t,n):r?m(n):p(n)}"function"==typeof define&&define.amd?define(function(){return A}):"object"==typeof module&&module.exports?module.exports=A:md5Function=A}(this);
+        !function (n) {
+            "use strict";
+
+            function t(n, t) {
+                var r = (65535 & n) + (65535 & t);
+                return (n >> 16) + (t >> 16) + (r >> 16) << 16 | 65535 & r
+            }
+
+            function r(n, t) {
+                return n << t | n >>> 32 - t
+            }
+
+            function e(n, e, o, u, c, f) {
+                return t(r(t(t(e, n), t(u, f)), c), o)
+            }
+
+            function o(n, t, r, o, u, c, f) {
+                return e(t & r | ~t & o, n, t, u, c, f)
+            }
+
+            function u(n, t, r, o, u, c, f) {
+                return e(t & o | r & ~o, n, t, u, c, f)
+            }
+
+            function c(n, t, r, o, u, c, f) {
+                return e(t ^ r ^ o, n, t, u, c, f)
+            }
+
+            function f(n, t, r, o, u, c, f) {
+                return e(r ^ (t | ~o), n, t, u, c, f)
+            }
+
+            function i(n, r) {
+                n[r >> 5] |= 128 << r % 32, n[14 + (r + 64 >>> 9 << 4)] = r;
+                var e, i, a, d, h, l = 1732584193, g = -271733879, v = -1732584194, m = 271733878;
+                for (e = 0; e < n.length; e += 16) i = l, a = g, d = v, h = m, g = f(g = f(g = f(g = f(g = c(g = c(g = c(g = c(g = u(g = u(g = u(g = u(g = o(g = o(g = o(g = o(g, v = o(v, m = o(m, l = o(l, g, v, m, n[e], 7, -680876936), g, v, n[e + 1], 12, -389564586), l, g, n[e + 2], 17, 606105819), m, l, n[e + 3], 22, -1044525330), v = o(v, m = o(m, l = o(l, g, v, m, n[e + 4], 7, -176418897), g, v, n[e + 5], 12, 1200080426), l, g, n[e + 6], 17, -1473231341), m, l, n[e + 7], 22, -45705983), v = o(v, m = o(m, l = o(l, g, v, m, n[e + 8], 7, 1770035416), g, v, n[e + 9], 12, -1958414417), l, g, n[e + 10], 17, -42063), m, l, n[e + 11], 22, -1990404162), v = o(v, m = o(m, l = o(l, g, v, m, n[e + 12], 7, 1804603682), g, v, n[e + 13], 12, -40341101), l, g, n[e + 14], 17, -1502002290), m, l, n[e + 15], 22, 1236535329), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 1], 5, -165796510), g, v, n[e + 6], 9, -1069501632), l, g, n[e + 11], 14, 643717713), m, l, n[e], 20, -373897302), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 5], 5, -701558691), g, v, n[e + 10], 9, 38016083), l, g, n[e + 15], 14, -660478335), m, l, n[e + 4], 20, -405537848), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 9], 5, 568446438), g, v, n[e + 14], 9, -1019803690), l, g, n[e + 3], 14, -187363961), m, l, n[e + 8], 20, 1163531501), v = u(v, m = u(m, l = u(l, g, v, m, n[e + 13], 5, -1444681467), g, v, n[e + 2], 9, -51403784), l, g, n[e + 7], 14, 1735328473), m, l, n[e + 12], 20, -1926607734), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 5], 4, -378558), g, v, n[e + 8], 11, -2022574463), l, g, n[e + 11], 16, 1839030562), m, l, n[e + 14], 23, -35309556), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 1], 4, -1530992060), g, v, n[e + 4], 11, 1272893353), l, g, n[e + 7], 16, -155497632), m, l, n[e + 10], 23, -1094730640), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 13], 4, 681279174), g, v, n[e], 11, -358537222), l, g, n[e + 3], 16, -722521979), m, l, n[e + 6], 23, 76029189), v = c(v, m = c(m, l = c(l, g, v, m, n[e + 9], 4, -640364487), g, v, n[e + 12], 11, -421815835), l, g, n[e + 15], 16, 530742520), m, l, n[e + 2], 23, -995338651), v = f(v, m = f(m, l = f(l, g, v, m, n[e], 6, -198630844), g, v, n[e + 7], 10, 1126891415), l, g, n[e + 14], 15, -1416354905), m, l, n[e + 5], 21, -57434055), v = f(v, m = f(m, l = f(l, g, v, m, n[e + 12], 6, 1700485571), g, v, n[e + 3], 10, -1894986606), l, g, n[e + 10], 15, -1051523), m, l, n[e + 1], 21, -2054922799), v = f(v, m = f(m, l = f(l, g, v, m, n[e + 8], 6, 1873313359), g, v, n[e + 15], 10, -30611744), l, g, n[e + 6], 15, -1560198380), m, l, n[e + 13], 21, 1309151649), v = f(v, m = f(m, l = f(l, g, v, m, n[e + 4], 6, -145523070), g, v, n[e + 11], 10, -1120210379), l, g, n[e + 2], 15, 718787259), m, l, n[e + 9], 21, -343485551), l = t(l, i), g = t(g, a), v = t(v, d), m = t(m, h);
+                return [l, g, v, m]
+            }
+
+            function a(n) {
+                var t, r = "", e = 32 * n.length;
+                for (t = 0; t < e; t += 8) r += String.fromCharCode(n[t >> 5] >>> t % 32 & 255);
+                return r
+            }
+
+            function d(n) {
+                var t, r = [];
+                for (r[(n.length >> 2) - 1] = void 0, t = 0; t < r.length; t += 1) r[t] = 0;
+                var e = 8 * n.length;
+                for (t = 0; t < e; t += 8) r[t >> 5] |= (255 & n.charCodeAt(t / 8)) << t % 32;
+                return r
+            }
+
+            function h(n) {
+                return a(i(d(n), 8 * n.length))
+            }
+
+            function l(n, t) {
+                var r, e, o = d(n), u = [], c = [];
+                for (u[15] = c[15] = void 0, o.length > 16 && (o = i(o, 8 * n.length)), r = 0; r < 16; r += 1) u[r] = 909522486 ^ o[r], c[r] = 1549556828 ^ o[r];
+                return e = i(u.concat(d(t)), 512 + 8 * t.length), a(i(c.concat(e), 640))
+            }
+
+            function g(n) {
+                var t, r, e = "";
+                for (r = 0; r < n.length; r += 1) t = n.charCodeAt(r), e += "0123456789abcdef".charAt(t >>> 4 & 15) + "0123456789abcdef".charAt(15 & t);
+                return e
+            }
+
+            function v(n) {
+                return unescape(encodeURIComponent(n))
+            }
+
+            function m(n) {
+                return h(v(n))
+            }
+
+            function p(n) {
+                return g(m(n))
+            }
+
+            function s(n, t) {
+                return l(v(n), v(t))
+            }
+
+            function C(n, t) {
+                return g(s(n, t))
+            }
+
+            function A(n, t, r) {
+                return t ? r ? s(t, n) : C(t, n) : r ? m(n) : p(n)
+            }
+
+            "function" == typeof define && define.amd ? define(function () {
+                return A
+            }) : "object" == typeof module && module.exports ? module.exports = A : md5Function = A
+        }(this);
 
         // 使用MD5函数处理输入
         return md5Function(input);
@@ -82,7 +178,7 @@
         if (hjfCount === hjmCount) {
             // 数据量相同时，用户可以互相查看对方的所有内容
             return (requestedAuthor === 'hjf' && authenticatedAuthor === 'hjm') ||
-                   (requestedAuthor === 'hjm' && authenticatedAuthor === 'hjf');
+                (requestedAuthor === 'hjm' && authenticatedAuthor === 'hjf');
         }
 
         // 确定数据量高低
@@ -97,6 +193,12 @@
             // 索引小的为新数据，所以要检查pageIndex是否在允许范围内
             const allowedStartIndex = higherCount - lowerCount; // 允许查看的起始索引
             return pageIndex >= allowedStartIndex;
+        }
+
+        // 如果请求的是数据量低的用户内容，且认证用户是数据量高的用户
+        if (requestedAuthor === lowerDataAuthor && authenticatedAuthor === higherDataAuthor) {
+            // 数据量高的用户可以查看数据量低的用户的所有内容
+            return true;
         }
 
         return false;
@@ -248,37 +350,52 @@
 
             if (hjfCount === hjmCount) {
                 // 数据量相等时，允许互相查看
-                const otherAuthor = author === 'hjf' ? 'hjm' : 'hjf';
                 canCrossAuth = true;
+                /*const otherAuthor = author === 'hjf' ? 'hjm' : 'hjf';
                 const crossAuthHint = document.createElement('p');
                 crossAuthHint.textContent = `提示: 您也可以输入${otherAuthor}的密码来访问此内容`;
                 crossAuthHint.style.margin = '5px 0 0 0';
                 crossAuthHint.style.fontSize = '12px';
                 crossAuthHint.style.fontStyle = 'italic';
                 crossAuthHint.style.color = '#1a2980';
-                crossAuthHint.style.display = 'none';
+                crossAuthHint.style.display = 'block';
                 hint.appendChild(document.createElement('br'));
-                hint.appendChild(crossAuthHint);
+                hint.appendChild(crossAuthHint);*/
             } else if (hjfCount !== hjmCount) {
                 const higherDataAuthor = hjfCount > hjmCount ? 'hjf' : 'hjm';
                 const lowerDataAuthor = hjfCount > hjmCount ? 'hjm' : 'hjf';
                 const higherCount = Math.max(hjfCount, hjmCount);
                 const lowerCount = Math.min(hjfCount, hjmCount);
 
+                // 如果请求的是数据量多的用户内容，且用户是数据量少的一方
                 if (author === higherDataAuthor) {
                     const allowedStartIndex = higherCount - lowerCount;
                     if (pageIndex >= allowedStartIndex) {
                         canCrossAuth = true;
-                        const crossAuthHint = document.createElement('p');
+                       /* const crossAuthHint = document.createElement('p');
                         crossAuthHint.textContent = `提示: 您也可以输入${lowerDataAuthor}的密码来访问此内容`;
                         crossAuthHint.style.margin = '5px 0 0 0';
                         crossAuthHint.style.fontSize = '12px';
                         crossAuthHint.style.fontStyle = 'italic';
                         crossAuthHint.style.color = '#1a2980';
-                        crossAuthHint.style.display = 'none';
+                        crossAuthHint.style.display = 'block';
                         hint.appendChild(document.createElement('br'));
-                        hint.appendChild(crossAuthHint);
+                        hint.appendChild(crossAuthHint);*/
                     }
+                }
+
+                // 如果请求的是数据量少的用户内容，且用户是数据量多的一方
+                if (author === lowerDataAuthor) {
+                    canCrossAuth = true;
+                    /*const crossAuthHint = document.createElement('p');
+                    crossAuthHint.textContent = `提示: 您也可以输入${higherDataAuthor}的密码来访问此内容`;
+                    crossAuthHint.style.margin = '5px 0 0 0';
+                    crossAuthHint.style.fontSize = '12px';
+                    crossAuthHint.style.fontStyle = 'italic';
+                    crossAuthHint.style.color = '#1a2980';
+                    crossAuthHint.style.display = 'block';
+                    hint.appendChild(document.createElement('br'));
+                    hint.appendChild(crossAuthHint);*/
                 }
             }
         }
@@ -400,13 +517,36 @@
                 const hjfCount = (window.NOTES_DATA.hjf || []).length;
                 const hjmCount = (window.NOTES_DATA.hjm || []).length;
 
+                // 数据量相等时，互相验证
+                if (hjfCount === hjmCount) {
+                    const otherAuthor = author === 'hjf' ? 'hjm' : 'hjf';
+                    if (verifyPassword(otherAuthor, password)) {
+                        storePasswordPrefix(password);
+                        setAuthenticated(otherAuthor);
+                        cleanupModal();
+                        callback(true);
+                        return;
+                    }
+                }
+
+                // 数据量不等时，交叉验证
                 if (hjfCount !== hjmCount) {
+                    const higherDataAuthor = hjfCount > hjmCount ? 'hjf' : 'hjm';
                     const lowerDataAuthor = hjfCount > hjmCount ? 'hjm' : 'hjf';
 
-                    if (verifyPassword(lowerDataAuthor, password)) {
-                        // 验证成功，存储密码前三位
+                    // 如果当前用户是数据量少的，尝试数据量多的用户密码
+                    if (author === higherDataAuthor && verifyPassword(lowerDataAuthor, password)) {
                         storePasswordPrefix(password);
                         setAuthenticated(lowerDataAuthor);
+                        cleanupModal();
+                        callback(true);
+                        return;
+                    }
+
+                    // 如果当前用户是数据量多的，尝试数据量少的用户密码
+                    if (author === lowerDataAuthor && verifyPassword(higherDataAuthor, password)) {
+                        storePasswordPrefix(password);
+                        setAuthenticated(higherDataAuthor);
                         cleanupModal();
                         callback(true);
                         return;
